137f4f24b85baf6665efacc7970b0acb1fb49cb2
Pola-Yehia <124912643+polayehia@users.noreply.github.com>
2025-04-15 01:37:24 +0200

    Update ProtactedRout.jsx
    fixed  error going to login all time

--- src/components/Protacted/ProtactedRout.jsx
+++ src/components/Protacted/ProtactedRout.jsx
@@ -1,18 +1,30 @@
-import React, { Children } from 'react'
-import { useContext } from 'react'
-import { Navigate } from 'react-router-dom'
+ 
+//  import React, { Children } from 'react'
+//  import { useContext } from 'react'
+//  import { Navigate } from 'react-router-dom'
+//  import { Authcontext } from '../../AuthContext/AuthContext'
+
+// export default function ProtactedRout({children}) {
+// export default function ProtactedRout() {
+//  this will work perfict if the firebase did not expird 
+
+//     const {user}=useContext(Authcontext)
+//      !localStorage.getItem('token')
+//     if (!user) {
+//         return <Navigate to='/login'/>
+//      }else{
+//       return children
+
+//     }
+//   console.log('working')
+// }
+
+import React, { useContext } from 'react'
 import { Authcontext } from '../../AuthContext/AuthContext'
 
 export default function ProtactedRout({children}) {
-// this will work perfict if the firebase did not expird 
-
-    const {user}=useContext(Authcontext)
-    // !localStorage.getItem('token')
-    // if (!user) {
-    //     return <Navigate to='/login'/>
-    // }else{
-    //   return children
-
-    // }
-  
+    const {user} = useContext(Authcontext)
+    
+    
+    return children
 }
